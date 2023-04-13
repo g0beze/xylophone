90a1e4b06ac2d0910e82b7f5183bd529bbab5029
MENDEX <39565143+M3ND3X@users.noreply.github.com>
2023-04-13 20:18:57 +0300

    applied each button

--- lib/main.dart
+++ lib/main.dart
@@ -13,17 +13,122 @@ class XylophoneApp extends StatelessWidget {
     return MaterialApp(
       home: Scaffold(
         body: SafeArea(
-          child: Center(
-            child: TextButton(
-              onPressed: () {
-                final player = AudioPlayer();
-                player.setSource(AssetSource('assets/assets_note1.wav'));
-              },
-              child: const Text('Click Me'),
-            ),
+          child: Column(
+            children: [
+              // for (int i = 1; i < 8; i++)
+                TextButton(
+                  onPressed: () {
+                    final player = AudioPlayer();
+                    player.setSource(AssetSource('assets_note1.wav'));
+                  },
+                  child: const Text('Click Me'),
+                ),
+                                TextButton(
+                  onPressed: () {
+                    final player = AudioPlayer();
+                    player.setSource(AssetSource('assets_note1.wav'));
+                  },
+                  child: const Text('Click Me'),
+                ),
+                                TextButton(
+                  onPressed: () {
+                    final player = AudioPlayer();
+                    player.setSource(AssetSource('assets_note1.wav'));
+                  },
+                  child: const Text('Click Me'),
+                ),
+                                TextButton(
+                  onPressed: () {
+                    final player = AudioPlayer();
+                    player.setSource(AssetSource('assets_note1.wav'));
+                  },
+                  child: const Text('Click Me'),
+                ),
+                                TextButton(
+                  onPressed: () {
+                    final player = AudioPlayer();
+                    player.setSource(AssetSource('assets_note1.wav'));
+                  },
+                  child: const Text('Click Me'),
+                ),
+                                TextButton(
+                  onPressed: () {
+                    final player = AudioPlayer();
+                    player.setSource(AssetSource('assets_note1.wav'));
+                  },
+                  child: const Text('Click Me'),
+                ),
+                                TextButton(
+                  onPressed: () {
+                    final player = AudioPlayer();
+                    player.setSource(AssetSource('assets_note1.wav'));
+                  },
+                  child: const Text('Click Me'),
+                ),
+                                TextButton(
+                  onPressed: () {
+                    final player = AudioPlayer();
+                    player.setSource(AssetSource('assets_note1.wav'));
+                  },
+                  child: const Text('Click Me'),
+                ),
+            ],
           ),
         ),
       ),
     );
   }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/* import 'package:flutter/material.dart';
+import 'package:audioplayers/audioplayers.dart';
+
+void main() {
+  runApp(const XylophoneApp());
+}
+
+class XylophoneApp extends StatelessWidget {
+  const XylophoneApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        body: SafeArea(
+          child: Column(
+            children: [
+              TextButton(
+                onPressed: () {
+                  final player = AudioPlayer();
+                  player.setSource(AssetSource('assets_note1.wav'));
+                },
+                child: const Text('Click Me'),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+} */
