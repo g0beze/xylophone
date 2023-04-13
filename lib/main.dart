1dd38fbb05bbe28a0d2a253f3f9aee514e2982a8
MENDEX <39565143+M3ND3X@users.noreply.github.com>
2023-04-14 00:56:52 +0300

    minimized code using lists

--- lib/main.dart
+++ lib/main.dart
@@ -10,82 +10,32 @@ class XylophoneApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    List<Color> colors = [
+      Colors.redAccent,
+      Colors.orangeAccent,
+      Colors.yellowAccent,
+      Colors.greenAccent,
+      Colors.black12,
+      Colors.blueAccent,
+      Colors.purpleAccent
+    ];
+
     return MaterialApp(
       home: Scaffold(
         body: SafeArea(
           child: Column(
             children: [
-              // for (int i = 1; i < 8; i++)
-              TextButton(
-                onPressed: () {
-                  final player = AudioPlayer();
-                  player.play(AssetSource('assets_note1.wav'));
-                },
-                style: TextButton.styleFrom(
-                  backgroundColor: Colors.redAccent,
+              for (int i = 1; i < 8; i++)
+                TextButton(
+                  onPressed: () {
+                    final player = AudioPlayer();
+                    player.play(AssetSource('assets_note$i.wav'));
+                  },
+                  style: TextButton.styleFrom(
+                    backgroundColor: colors[i - 1],
+                  ),
+                  child: const Text(''),
                 ),
-                child: const Text(''),
-              ),
-              TextButton(
-                onPressed: () {
-                  final player = AudioPlayer();
-                  player.play(AssetSource('assets_note2.wav'));
-                },
-                style: TextButton.styleFrom(
-                  backgroundColor: Colors.orangeAccent,
-                ),
-                child: const Text(''),
-              ),
-              TextButton(
-                onPressed: () {
-                  final player = AudioPlayer();
-                  player.play(AssetSource('assets_note3.wav'));
-                },
-                style: TextButton.styleFrom(
-                  backgroundColor: Colors.yellowAccent,
-                ),
-                child: const Text(''),
-              ),
-              TextButton(
-                onPressed: () {
-                  final player = AudioPlayer();
-                  player.play(AssetSource('assets_note4.wav'));
-                },
-                style: TextButton.styleFrom(
-                  backgroundColor: Colors.greenAccent,
-                ),
-                child: const Text(''),
-              ),
-              TextButton(
-                onPressed: () {
-                  final player = AudioPlayer();
-                  player.play(AssetSource('assets_note5.wav'));
-                },
-                style: TextButton.styleFrom(
-                  backgroundColor: Colors.black12,
-                ),
-                child: const Text(''),
-              ),
-              TextButton(
-                onPressed: () {
-                  final player = AudioPlayer();
-                  player.play(AssetSource('assets_note6.wav'));
-                },
-                style: TextButton.styleFrom(
-                  backgroundColor: Colors.blueAccent,
-                ),
-                child: const Text(''),
-              ),
-              TextButton(
-                onPressed: () {
-                  final player = AudioPlayer();
-                  player.play(AssetSource('assets_note7.wav'));
-                },
-                style: TextButton.styleFrom(
-                  backgroundColor: Colors.purpleAccent,
-                ),
-                child: const Text(''),
-              ),
             ],
           ),
         ),
